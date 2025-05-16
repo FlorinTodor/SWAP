69c8cd49e88bae4aca89e6739031b16ecfa8e7c2
Florin Emanuel Todor Gliga <florinemanueltodor@gmail.com>
2025-05-17 01:50:39 +0200

    P5 AB finished

---
+++ P5-granjaweb/P5-flotodor-apache/P5-flotodor-iptables-web/flotodor-iptables-web.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Script de configuración de IPTABLES para contenedores web
+# Autor: Florin Emanuel Todor Gliga
+
+# IP del balanceador de carga (ajustar si cambia)
+
+
+BALANCER_IP="192.168.10.50"
+
+# Establecer políticas por defecto (Denegación implícita), es decir, denegar todo el tráfico entrante y saliente , ya que no tenemos reglas de aceptación
+iptables -P INPUT DROP
+iptables -P OUTPUT DROP
+iptables -P FORWARD DROP
+
+# Permitir tráfico en la interfaz de loopback (localhost)
+iptables -A INPUT -i lo -j ACCEPT
+iptables -A OUTPUT -o lo -j ACCEPT
+
+# Permitir tráfico de conexiones ya establecidas o relacionadas (entrante)
+iptables -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
+
+# Permitir tráfico de conexiones nuevas, establecidas y relacionadas (saliente)
+iptables -A OUTPUT -m state --state NEW,ESTABLISHED,RELATED -j ACCEPT
+
+# Permitir tráfico HTTP (puerto 80) desde el balanceador
+iptables -A INPUT -p tcp -s $BALANCER_IP --dport 80 -j ACCEPT
+
+# Permitir tráfico HTTPS (puerto 443) desde el balanceador
+iptables -A INPUT -p tcp -s $BALANCER_IP --dport 443 -j ACCEPT
+
